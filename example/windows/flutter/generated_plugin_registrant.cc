777d46b022702c628149a05fe3b6a73e51b9d65e
Abner Silva de Oliveira <abner.silva@gmail.com>
2021-04-02 08:38:50 -0300

    added windows support to example app

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_qjs/flutter_qjs_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterQjsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterQjsPlugin"));
+}
---
+++ windows/README.md
@@ -0,0 +1,10 @@
+
+
+## Tutorial
+
+https://dzone.com/articles/build-great-windows-desktop-apps-with-flutter
+
+## Visual Studio Download
+
+https://visualstudio.microsoft.com/pt-br/thank-you-downloading-visual-studio/?sku=Community&rel=16
+
