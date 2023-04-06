43db5dd24dbd600bb1f241d3108eed6ac28d43d7
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2023-04-06 19:58:36 -0300

    Update flutter_js.dart

--- lib/flutter_js.dart
+++ lib/flutter_js.dart
@@ -171,8 +171,6 @@ class FlutterJs {
     };
     final rs = await _methodChannel.invokeMethod("evaluate", arguments);
     
-    print('RS: $rs');
-    
     final String? jsResult = rs is Map || rs is List ? json.encode(rs) : rs;
     if (DEBUG) {
       print("${DateTime.now().toIso8601String()} - JS RESULT : $jsResult");
